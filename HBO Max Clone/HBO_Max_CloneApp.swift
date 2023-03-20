9ca370bd009e8b63abd92a72f6d988261c7dfad3
crwells86 <calebrwells@gmail.com>
2023-03-20 09:39:20 -0700

    Initial Commit

---
+++ HBO Max Clone/HBO_Max_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HBO_Max_CloneApp.swift
+//  HBO Max Clone
+//
+//  Created by Caleb Wells on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct HBO_Max_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
